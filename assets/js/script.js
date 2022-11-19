e42ab266a0b6f29902054220af21bd467e88c468
lorenzo123451 <118128221+lorenzo123451@users.noreply.github.com>
2022-11-19 10:45:45 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const topBtn = document.querySelector(".to-top-btn");
+
+window.addEventListener('scroll' , () => {
+    if (window.pageYOffset > 400) {
+        topBtn.classList.add("active");
+    } else {
+        topBtn.classList.remove("active");
+    }
+})
